75d497655ca78261b84c3b35e34cb432ab278cf7
Mardionocs <mardionoputra19>
2020-03-21 02:27:23 +0700

    TUGAS 14 JS
    Tugas dari Niomic

---
+++ tugas14.js
@@ -0,0 +1,11 @@
+function tugasMath() {
+  var a = 37;
+  var b = 24;
+
+  var x = a/b;
+  console.log(x);
+  console.log(Math.round(x));
+  console.log(Math.ceil(x));
+  console.log(Math.floor(x));
+}
+tugasMath()
